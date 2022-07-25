440a3cc742054dce6ba21b2ddf4b695759916650
啟翔 黃 <max74107410@gmail.com>
2022-07-25 18:16:16 +0800

    07251616

--- qqqq/Controllers/petsController.cs
+++ qqqq/Controllers/petsController.cs
@@ -7,6 +7,7 @@ using qqqq.Models;
 using qqqq.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace qqqq.Controllers
     public class petsController : Controller
     {
         我救浪Context db = new 我救浪Context();
+        List<CAdoptView> list = new List<CAdoptView>();
         public readonly IWebHostEnvironment _enviroment;
         public petsController(IWebHostEnvironment p)
         {
@@ -24,82 +26,135 @@ namespace qqqq.Controllers
 
         public IActionResult petsList()
         {
-            var q = db.Products.AsEnumerable().Where(p => p.IsPet == true).ToList();
-            return View(CProductView.CProductViews(q));
+            //var a = db.Products.Where(x => x.IsPet == true);
+            //List<CAdoptView> list = new List<CAdoptView>();
+            //////a.FirstOrDefault().SubCategory.SubCategoryName;
+            //foreach (var i in a)
+            //{
+            //    CAdoptView adoptView = new CAdoptView();
+            //    adoptView._prod = i;
+            //    list.Add(adoptView);
+            //}
+            //Debug.WriteLine(list[0]._prod.Photos.FirstOrDefault().PictureName);
+            return View(/*list*/);
+        }
+        public IActionResult petsPhoto()
+        {
+            var a = db.Products.Where(x => x.IsPet == true);
+            
+            ////a.FirstOrDefault().SubCategory.SubCategoryName;
+            foreach (var i in a)
+            {
+                CAdoptView adoptView = new CAdoptView();
+                adoptView._prod = i;
+                list.Add(adoptView);
+            }
+            return PartialView("petsPhoto",list);
         }
         public IActionResult petsSubcategory(int id)
         {
-            var db = new 我救浪Context();
-            var list = db.SubCategories.Where(p => p.CategoryId == id || p.SubCategoryName == "不限").Select(p=>new { SubCategoryId =p.SubCategoryId,SubCategoryName=p.SubCategoryName }).ToList();
+            var list = db.SubCategories.Where(p => p.CategoryId == id || p.SubCategoryName == "不限").Select(p => new { SubCategoryId = p.SubCategoryId, SubCategoryName = p.SubCategoryName }).ToList();
             var jsonsubpet = JsonSerializer.Serialize(list);
             return Json(jsonsubpet);
         }
-
-        public IActionResult Search(string keyword)
+        public IActionResult SearchForPet(PetDetail petDetail,Product product,int categoryid)
         {
-
-            return View();
+            var a = db.Products.Where(p => (product.SubCategoryId == 1 ? true : p.SubCategoryId == product.SubCategoryId) &&
+                                    (p.IsPet==true)&&
+                                    (categoryid==1?true:p.SubCategory.CategoryId==categoryid)&&
+                                    (petDetail.GenderId == 1 ? true : p.PetDetail.GenderId == petDetail.GenderId) &&
+                                    (petDetail.SizeId == 1 ? true : p.PetDetail.SizeId == petDetail.SizeId)).ToList();
+           foreach(var i  in a)
+            {
+                CAdoptView adoptView = new CAdoptView();
+                adoptView._prod = i;
+                list.Add(adoptView);
+            }
+            return PartialView("petsPhoto", list);
+        }
+        public IActionResult SearchForKeyword(string keyword)
+        {
+            if (String.IsNullOrEmpty(keyword))
+            {
+                return petsPhoto();
+            }
+            else
+            {
+                var a = db.Products.Where(p => p.IsPet == true && (p.ProductName.Contains(keyword) || p.SubCategory.SubCategoryName.Contains(keyword) || p.SubCategory.Category.CategoryName.Contains(keyword) || p.Description.Contains(keyword))).ToList();
+                foreach (var i in a)
+                {
+                    CAdoptView adoptView = new CAdoptView();
+                    adoptView._prod = i;
+                    list.Add(adoptView);
+                }
+                Debug.WriteLine(list[0].GenderType);
+                return PartialView("petsPhoto", list);
+            }
+            
         }
         public IActionResult petsDetial(int id)
         {
+          
+            var a = db.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            
             //CProductShow CprodShow = new CProductShow();
             //Product cProd = (new 我救浪Context()).Products.FirstOrDefault(p => p.ProductId == id);
             //CprodShow.product = cProd;
             //if (cProd.Photos.Any())
             //    CprodShow.Photos = cProd.Photos.ToList();
             //return View(CprodShow);
-            return View();
+            return View(a);
         }
 
-        public IActionResult petsPhoto()
-        {
-            我救浪Context db = new 我救浪Context();
-            var datas = db.Products.Where(p => p.IsPet == true).ToList();
-            List<CProductShow> list = new List<CProductShow>();
-            foreach (Product p in datas)
-            {
-                CProductShow cprod = new CProductShow();
-                cprod.product = p;
-                if (p.Photos.Any())
-                    cprod.Photos = p.Photos.ToList();
-                list.Add(cprod);
-            }
-            return PartialView(list);
-        }
-        public IActionResult petsPhotoRow()
-        {
-            我救浪Context db = new 我救浪Context();
-            var datas = db.Products.Where(p => p.IsPet == true).ToList();
-            List<CProductShow> list = new List<CProductShow>();
-            foreach (Product p in datas)
-            {
-                CProductShow cprod = new CProductShow();
-                cprod.product = p;
-                if (p.Photos.Any())
-                    cprod.Photos = p.Photos.ToList();
-                list.Add(cprod);
-            }
-            return PartialView(list);
-        }
-        public IActionResult petsAdopt()
-        {
-            return View();
-        }
-        public IActionResult petsMatch()
-        {
-            我救浪Context db = new 我救浪Context();
-            var datas = db.Products.Where(p => p.IsPet == true).ToList();
-            List<CProductShow> list = new List<CProductShow>();
-            foreach (Product p in datas)
-            {
-                CProductShow cprod = new CProductShow();
-                cprod.product = p;
-                if (p.Photos.Any())
-                    cprod.Photos = p.Photos.ToList();
-                list.Add(cprod);
-            }
-            return View(list);
-        }
-    }   
+        //    public IActionResult petsPhoto()
+        //    {
+        //        我救浪Context db = new 我救浪Context();
+        //        var datas = db.Products.Where(p => p.IsPet == true).ToList();
+
+        //        //foreach (Product p in datas)
+        //        //{
+        //        //    CPet cprod = new CPet();
+        //        //    cprod._prod = p;
+        //        //    if (p.Photos.Any())
+        //        //        cprod.Photos = p.Photos.ToList();
+        //        //    list.Add(cprod);
+        //        //}
+        //        return PartialView(list);
+        //    }
+        //    public IActionResult petsPhotoRow()
+        //    {
+        //        我救浪Context db = new 我救浪Context();
+        //        var datas = db.Products.Where(p => p.IsPet == true).ToList();
+        //        List<CProductShow> list = new List<CProductShow>();
+        //        foreach (Product p in datas)
+        //        {
+        //            CProductShow cprod = new CProductShow();
+        //            cprod.product = p;
+        //            if (p.Photos.Any())
+        //                cprod.Photos = p.Photos.ToList();
+        //            list.Add(cprod);
+        //        }
+        //        return PartialView(list);
+        //    }
+        //    public IActionResult petsAdopt()
+        //    {
+        //        return View();
+        //    }
+        //    public IActionResult petsMatch()
+        //    {
+        //        我救浪Context db = new 我救浪Context();
+        //        var datas = db.Products.Where(p => p.IsPet == true).ToList();
+        //        List<CProductShow> list = new List<CProductShow>();
+        //        foreach (Product p in datas)
+        //        {
+        //            CProductShow cprod = new CProductShow();
+        //            cprod.product = p;
+        //            if (p.Photos.Any())
+        //                cprod.Photos = p.Photos.ToList();
+        //            list.Add(cprod);
+        //        }
+        //        return View(list);
+        //    }
+    }
 }
 
---
+++ qqqq/ViewModels/CAdoptView.cs
@@ -0,0 +1,141 @@
+﻿using qqqq.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qqqq.ViewModels
+{
+    public class CAdoptView
+    {
+        public Category _category;
+        public SubCategory _subCategory;
+        public Product _prod;
+        public PetDetail _petDetail;
+        public Gender _gender;
+        public Color _color;
+        public City _city;
+        public Size _size;
+        public Age _age;
+        public Ligation _ligation;
+        public Photo _photo;
+
+
+
+
+        public CAdoptView()
+        {
+            _category = new Category();
+            _subCategory = new SubCategory();
+            _prod = new Product();
+            _petDetail = new PetDetail();
+            _gender = new Gender();
+            _color = new Color();
+            _city = new City();
+            _size = new Size();
+            _age = new Age();
+            _ligation = new Ligation();
+            _photo = new Photo();
+
+        }
+
+
+        //性別--
+        public string GenderType
+        {
+            get { return _prod.PetDetail.Gender.GenderType; }
+        }
+
+        //顏色--
+        public string ColorName
+        {
+            get { return _prod.PetDetail.Color.ColorName; }
+        }
+
+        //城市--
+        public string CityName
+        {
+            get { return _prod.PetDetail.City.CityName; }
+        }
+
+        //體型 -- 
+        public string SizeType
+        {
+            get { return _prod.PetDetail.Size.SizeType; }
+        }
+
+        //年紀--
+        public string AgeType
+        {
+            get { return _prod.PetDetail.Age.AgeType; }
+        }
+
+        //結紮--
+        public string LigationType
+        {
+            get { return _prod.PetDetail.Ligation.LigationType; }
+        }
+
+
+
+        //category --
+        [DisplayName("類別")]
+        public string CategoryName
+        {
+            get { return _prod.SubCategory.Category.CategoryName; }
+        }
+        //subcategory --
+        [DisplayName("品種")]
+        public string SubCategoryName
+        {
+            get { return _prod.SubCategory.SubCategoryName; }
+        }
+        //product
+        public int ProductId
+        {
+            get { return _prod.ProductId; }
+        }
+        [DisplayName("名字")]
+        public string ProductName
+        {
+            get { return _prod.ProductName; }
+        }
+        [DisplayName("描述")]
+        public string Description
+        {
+            get { return _prod.Description; }
+        }
+        public int? UnitsInStock
+        {
+            get { return _prod.UnitsInStock; }
+        }
+        [DisplayName("領養狀態")]
+        public bool? Continued
+        {
+            get { return _prod.Continued; }
+        }
+        public decimal? Cost
+        {
+            get { return _prod.Cost; }
+        }
+        //petdetail
+        [DisplayName("年花費")]
+        public decimal? YearCost
+        {
+            get { return _prod.PetDetail.YearCost; }
+        }
+        [DisplayName("空間")]
+        public int? Space
+        {
+            get { return _prod.PetDetail.Space; }
+        }
+        [DisplayName("週陪伴時間")]
+        public int? AccompanyTimeWeek
+        {
+            get { return _prod.PetDetail.AccompanyTimeWeek; }
+        }
+
+        public List<Photo> Photos { get; set; }
+    }
+}
