38e1a0aceb442e73db0316709b2f80ecebfc4feb
姿秀 林 <lss9999ch@yahoo.com.tw>
2022-08-10 03:32:23 +0800

    08/10

--- qqqq/Controllers/HomeController.cs
+++ qqqq/Controllers/HomeController.cs
@@ -188,19 +188,28 @@ namespace prjHomeLess_R.Controllers
             return View("Login");
 
         }
-        //public IActionResult checkPsw(string email, string pwd)
-        //{
-        //    var q = _context.Members.Where(m => m.Email == email && m.Password == pwd).FirstOrDefault();
+        public IActionResult checkPsw(string email, string pwd1, string pwd2)
+        {
+            if(pwd1 != pwd2)
+            {
+                return Json("n");
+            }
+            else
+            {
+                var q = _context.Members.Where(m => m.Email == email && m.Password == pwd1).FirstOrDefault();
+                if (q != null)
+                {
+                    return Json("q");
+                }
+                else
+                {
+                    return Json("");
+                }//todo
+            }
+          
 
-        //    if (q != null)
-        //    {
-        //        return Json("q");
-        //    }
-        //    else
-        //    {
-        //        return Json("");
-        //    }//todo
-        //}
+          
+        }
         public IActionResult sendMail(string email)
         {
             var a = _context.Members.Where(x => x.Email == email).Select(y => y.Email).FirstOrDefault();
@@ -215,6 +224,7 @@ namespace prjHomeLess_R.Controllers
                 mail.Subject = "我就浪忘記密碼密碼";
                 //內文
                 mail.Body = $"<html><h1>忘記密碼</h1><h4>請點擊以下連結以重新設定密碼</h4><body><a href='https://localhost:44318/home/forgetPwd?id={email}'>https://localhost:44318/home/forgetPwd</a></body></html>";
+               // mail.Body = $"<html><h1>忘記密碼</h1><h4>請點擊以下連結以重新設定密碼</h4><body><a href='http://192.168.21.37/home/forgetPwd?id={email}'>http://192.168.21.37/home/forgetPwd</a></body></html>";
 
                 //內文是否為html
                 mail.IsBodyHtml = true;
@@ -253,13 +263,16 @@ namespace prjHomeLess_R.Controllers
             ViewBag.mail = id;
             return View();
         }
+        public IActionResult forgetPwdlogined()
+        {
+            return View();
+        }
         public IActionResult setPwd(string email,string pwd)
         {
 
             Debug.WriteLine(email);
             Debug.WriteLine(pwd);
             var q = _context.Members.Where(m => m.Email == email).FirstOrDefault();
-          
                 q.Password = pwd;
                 _context.SaveChanges();
 
